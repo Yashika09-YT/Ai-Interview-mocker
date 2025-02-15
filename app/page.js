1a2cff01aa788b1364c57cd4e896573709cc8e61
Yashika Tanwar <tanwaryashika5@gmail.com>
2025-02-15 15:35:07 +0530

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+// import Image from "next/image";
+// import { Button } from "@/components/ui/button"
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Yashika Tanwar </h1>
+//       <Button variant="outline">Button</Button>
+
+//     </div>
+//   );
+// }
+
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Yashika Tanwar </h1>
-      <Button variant="outline">Button</Button>
-
-    </div>
-  );
+  redirect("/dashboard");
 }
